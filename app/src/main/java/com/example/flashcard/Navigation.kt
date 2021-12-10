35592eca8b8eecb98132d472aed8bb56c8277e8e
hoseinlook <hoseinlooki0@gmail.com>
2021-12-10 21:59:48 +0330

    refactor NavigationItem

--- app/src/main/java/com/example/flashcard/Navigation.kt
+++ app/src/main/java/com/example/flashcard/Navigation.kt
@@ -23,6 +23,11 @@ sealed class ScreenRoute(val route: String) {
 }
 
 
+sealed class NavigationItem(var route: String, var icon: Int, var title: String) {
+    object Home : NavigationItem("home", R.drawable.ic_home, "Home")
+    object Categories : NavigationItem("categories", R.drawable.ic_categories, "Categories")
+}
+
 @ExperimentalFoundationApi
 @Composable
 fun Navigation() {
--- app/src/main/java/com/example/flashcard/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/flashcard/components/BottomNavigationBar.kt
@@ -2,21 +2,18 @@ package com.example.flashcard.activities
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.flashcard.NavigationItem
 import com.example.flashcard.R
-import com.example.flashcard.components.NavigationItem
 
 @Composable
 fun BottomNavigationBar() {
@@ -31,7 +28,10 @@ fun BottomNavigationBar() {
         items.forEach { item ->
             BottomNavigationItem(
                 icon = {
-                    Column(horizontalAlignment = CenterHorizontally, verticalArrangement = Arrangement.Center ) {
+                    Column(
+                        horizontalAlignment = CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
                         Icon(
                             painterResource(id = item.icon),
                             contentDescription = item.title
