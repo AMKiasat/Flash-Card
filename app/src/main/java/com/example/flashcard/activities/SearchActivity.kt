53e421f01e1f5198e843e2222b133736878e54c0
AMKiasat <kiasatamirm@gmail.com>
2021-12-29 12:37:31 +0330

    Adding search activity

---
+++ app/src/main/java/com/example/flashcard/activities/SearchActivity.kt
@@ -0,0 +1,120 @@
+package com.example.flashcard.activities
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.flashcard.CATEGORY_LIST
+import com.example.flashcard.ScreenRoute
+import com.example.flashcard.components.CategoryCardListBox
+import kotlinx.coroutines.Dispatchers
+
+@ExperimentalFoundationApi
+@Composable
+fun SearchActivity(navController: NavController) {
+
+    Scaffold(topBar = { },
+
+        bottomBar = { BottomNavigationBar(navController = navController) }) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            SearchBar(
+                hint = "Search...",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+
+            }
+        }
+    }
+
+}
+
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {}
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    var isHintDisplayed by remember {
+        mutableStateOf(hint != "")
+    }
+
+    Box(modifier = modifier) {
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+                onSearch(it)
+            },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged {
+                    isHintDisplayed = it.isFocused != true
+                }
+        )
+        if(isHintDisplayed) {
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun searchList(query: String) {
+    val listToSearch = if(isSearchStarting) {
+        pokemonList.value
+    } else {
+        cachedPokemonList
+    }
+    viewModelScope.launch(Dispatchers.Default) {
+        if(query.isEmpty()) {
+            pokemonList.value = cachedPokemonList
+            isSearching.value = false
+            isSearchStarting = true
+            return@launch
+        }
+        val results = listToSearch.filter {
+            it.pokemonName.contains(query.trim(), ignoreCase = true) ||
+                    it.number.toString() == query.trim()
+        }
+        if(isSearchStarting) {
+            cachedPokemonList = pokemonList.value
+            isSearchStarting = false
+        }
+        pokemonList.value = results
+        isSearching.value = true
+    }
+}
