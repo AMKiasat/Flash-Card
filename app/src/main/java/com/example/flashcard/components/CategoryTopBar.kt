7284a91599106d85ce9ebce07dbb63fda3efa5b2
AMKiasat <kiasatamirm@gmail.com>
2021-12-30 18:56:26 +0330

    Adding top bar to inside category

---
+++ app/src/main/java/com/example/flashcard/components/CategoryTopBar.kt
@@ -0,0 +1,48 @@
+package com.example.flashcard.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.flashcard.R
+import com.example.flashcard.NavigationBarItems
+
+@Composable
+fun CategoryTopBar(navController: NavController, name: String) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "slm",
+                fontSize = 20.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(
+                    painterResource(id = R.drawable.ic_back),
+                    contentDescription = "Back"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    painterResource(id = R.drawable.ic_sync),
+                    contentDescription = "Sync"
+                )
+            }
+        }
+    )
+}
