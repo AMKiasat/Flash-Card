26f93403a0db87ecc97099a9057692f6679d95b0
hoseinlook <hoseinlooki0@gmail.com>
2021-12-30 13:02:20 +0330

    request a  permission from user

--- app/src/main/java/com/example/flashcard/MainActivity.kt
+++ app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.flashcard
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
@@ -17,12 +24,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             val permissionsState = rememberMultiplePermissionsState(
                 permissions = listOf(
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA,
                     Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.MANAGE_EXTERNAL_STORAGE,
                     Manifest.permission.INTERNET,
+                    Manifest.permission.MANAGE_MEDIA,
                 )
 
+            )
+            val lifecycleOwner = LocalLifecycleOwner.current
+            DisposableEffect(key1 = lifecycleOwner,
+                effect ={
+                    val observer =LifecycleEventObserver{_,event ->
+
+                        if (event== Lifecycle.Event.ON_START){
+                            permissionsState.launchMultiplePermissionRequest()
+                        }
+                    }
+                    lifecycleOwner.lifecycle.addObserver(observer)
+
+                    onDispose {
+                        lifecycleOwner.lifecycle.removeObserver(observer)
+                    }
+                }
             )
 
 
