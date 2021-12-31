6bc1f110cbb06d312498e018228ff83aa3eda461
hoseinlook <hoseinlooki0@gmail.com>
2021-12-31 21:45:40 +0330

    sync with api

---
+++ app/src/main/java/com/example/flashcard/Sync.kt
@@ -0,0 +1,40 @@
+package com.example.flashcard
+
+import CategoryApiViewModel
+import android.content.Context
+import com.example.flashcard.api.WordApi
+import com.example.flashcard.api.WordApiViewModel
+import com.example.flashcard.localDatabase.CategoryCardRepository
+import com.example.flashcard.localDatabase.FlashCardDatabase
+import com.example.flashcard.localDatabase.WordCardRepository
+
+
+fun syncStore(context: Context) {
+    val wordRep = WordCardRepository(FlashCardDatabase.getInstance(context = context).wordCardDao())
+    val categoryRep =
+        CategoryCardRepository(FlashCardDatabase.getInstance(context = context).categoryCardDao())
+    val wordApi = WordApiViewModel()
+    val categoryApi = CategoryApiViewModel()
+
+
+    val wordEntityList = wordRep.getAll()
+    val wordApiList = mutableListOf<WordApi>()
+
+    wordEntityList.forEach {
+        wordApiList += WordApi(
+            id = it.id,
+            pic_location = it.pic_location,
+            learned = it.learned,
+            rememberCount = it.rememberCount,
+            rememberType = it.rememberType,
+            category = it.category,
+            definition = it.definition,
+            lastRememberTime = it.lastRememberTime,
+            word = it.word
+        )
+    }
+
+    wordApi.storeWordList(wordApiList)
+
+
+}
--- app/src/main/java/com/example/flashcard/api/WordApi.kt
+++ app/src/main/java/com/example/flashcard/api/WordApi.kt
@@ -11,16 +11,17 @@ import androidx.room.PrimaryKey
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.GET
-
-
+import retrofit2.http.POST
 
 
 data class WordApi(
-    val id: Int,
-    val pic_location: String,
+    val id: Int?,
+    val pic_location: String?,
     val word: String,
     val definition: String,
+    val category: String?,
     val lastRememberTime: String,
     val rememberType: String,
     val rememberCount: Int,
@@ -32,6 +33,9 @@ interface ApiService {
 
     @GET("word")
     suspend fun getMovies() : List<WordApi>
+
+    @POST("word")
+    suspend fun setWords(@Body wordList: List<WordApi>):String
 
     companion object {
         var apiService: ApiService? = null
@@ -51,6 +55,7 @@ interface ApiService {
 class WordApiViewModel : ViewModel() {
     var movieListResponse:List<WordApi> by mutableStateOf(listOf())
     var errorMessage: String by mutableStateOf("")
+
     fun getWordList() {
         viewModelScope.launch {
             val apiService = ApiService.getInstance()
@@ -65,4 +70,22 @@ class WordApiViewModel : ViewModel() {
             }
         }
     }
+
+    fun storeWordList(wordList: List<WordApi>){
+        viewModelScope.launch {
+            val apiService = ApiService.getInstance()
+            try {
+                Log.d("wordApiList", "sending $wordList")
+                apiService.setWords(wordList = wordList)
+                Log.d("wordApiList", "send succses")
+
+            }
+            catch (e: Exception) {
+                errorMessage = e.message.toString()
+                Log.d("wordApiListERROR", "$errorMessage ")
+            }
+        }
+
+    }
+
 }
